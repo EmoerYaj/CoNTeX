a8879b100055a44ca176c21fb1523f1612ae6191
EmoerYaj <ciudad.rj01@gmail.com>
2024-06-15 22:55:28 +0800

    Add Login and sign up and logout

---
+++ Loginpage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="icon" href="./images/white-logo.svg">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/fonts.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/preview.css" rel="stylesheet">
+</head>
+<body>
+    
+<div class="table-container"> 
+    <table>
+        <tr>
+        <th><img src="" alt="image"></th>
+        <th>
+        <form method="post" action="validate.php">
+            <p>Email: <input type="text" name ="email"/></p>
+            <p>Password: <input type="password" name ="password"/></p>
+            <p><input type="checkbox" name="remember" value="1">Remember Me</p>
+            <p> <input type="submit" name="login" value="login"></p>
+            <a>Don't have an account?<a href="signup_process.php">Click Here</a>
+        </form>
+        </th>
+        </tr>
+    </table>
+</div>
+
+</body>
+</html>
